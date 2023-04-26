a7d5299f9bf575ef016db05478b7f06a7c848a4b
skanatbekov <delkanatus@gmail.com>
2023-04-26 16:03:07 +0600

    add Mentor model

--- online_tutor/models.py
+++ online_tutor/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    experience = models.CharField(max_length=50)
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return self.name

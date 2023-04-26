27e6fe263721baef8c9c0c16ed9f01b76a3ab800
Janna-git <zhannat.shayakunova@mail.ru>
2023-04-26 16:13:09 +0600

    added model Student

--- online_tutor/models.py
+++ online_tutor/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
 
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    birth_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

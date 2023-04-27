f32758c273d3a6b3009ac107b3baf04ef9a11d1f
skanatbekov <delkanatus@gmail.com>
2023-04-27 17:31:46 +0600

    deleted

--- online_tutor/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsPermitted(BasePermission):
-    def has_permission(self, request, view):
-        if request.user and request.user.is_staff:
-            return True
-        return False
